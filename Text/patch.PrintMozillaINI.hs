431b11f22b02726267f5b8418db35e8f3d2df218
James Crayne <jim.crayne@gmail.com>
2015-03-04 23:50:03 -0500

    more blank lines in pretty printer

--- Text/patch.PrintMozillaINI.hs
+++ Text/patch.PrintMozillaINI.hs
@@ -1,5 +1,5 @@
 diff --git a/Text/PrintMozillaINI.hs b/Text/PrintMozillaINI.hs
-index e8b47e3..058f588 100644
+index e8b47e3..9670105 100644
 --- a/Text/PrintMozillaINI.hs
 +++ b/Text/PrintMozillaINI.hs
 @@ -19,7 +19,7 @@ doc = (:)
@@ -7,11 +7,19 @@ index e8b47e3..058f588 100644
  render d = rend 0 (map ($ "") $ d []) "" where
    rend i ss = case ss of
 -    "["      :ts -> showChar '[' . rend i ts
-+    "["      :ts -> new i . showChar '[' . rend i ts
++    "["      :ts -> newnew i . showChar '[' . rend i ts
      "("      :ts -> showChar '(' . rend i ts
      "{"      :ts -> showChar '{' . new (i+1) . rend (i+1) ts
      "}" : ";":ts -> new (i-1) . space "}" . showChar ';' . new (i-1) . rend (i-1) ts
-@@ -97,7 +97,7 @@ instance Print Grp where
+@@ -31,6 +31,7 @@ render d = rend 0 (map ($ "") $ d []) "" where
+     t        :ts -> space t . rend i ts
+     _            -> id
+   new i   = showChar '\n' . replicateS (2*i) (showChar ' ') . dropWhile isSpace
++  newnew i = showChar '\n' . showChar '\n' . replicateS (2*i) (showChar ' ') . dropWhile isSpace
+   space t = showString t . (\s -> if null s then "" else (' ':s))
+ 
+ parenth :: Doc -> Doc
+@@ -97,7 +98,7 @@ instance Print Grp where
  
  instance Print KeyPair where
    prt i e = case e of
