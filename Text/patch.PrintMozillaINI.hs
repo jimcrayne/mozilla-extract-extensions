d97c333e7668422d29f8c4b5a1e9595106eb01c5
James Crayne <jim.crayne@gmail.com>
2015-03-04 23:46:18 -0500

    updating pretty printer

--- Text/patch.PrintMozillaINI.hs
+++ Text/patch.PrintMozillaINI.hs
@@ -1,7 +1,16 @@
 diff --git a/Text/PrintMozillaINI.hs b/Text/PrintMozillaINI.hs
-index e8b47e3..142e9fc 100644
+index e8b47e3..058f588 100644
 --- a/Text/PrintMozillaINI.hs
 +++ b/Text/PrintMozillaINI.hs
+@@ -19,7 +19,7 @@ doc = (:)
+ render :: Doc -> String
+ render d = rend 0 (map ($ "") $ d []) "" where
+   rend i ss = case ss of
+-    "["      :ts -> showChar '[' . rend i ts
++    "["      :ts -> new i . showChar '[' . rend i ts
+     "("      :ts -> showChar '(' . rend i ts
+     "{"      :ts -> showChar '{' . new (i+1) . rend (i+1) ts
+     "}" : ";":ts -> new (i-1) . space "}" . showChar ';' . new (i-1) . rend (i-1) ts
 @@ -97,7 +97,7 @@ instance Print Grp where
  
  instance Print KeyPair where
